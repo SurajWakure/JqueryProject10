ac591ceb380b56f88ac67b108d5148420e4d9808
suraj <surajwakure007@gmail.com>
2024-10-06 21:26:53 +0530

    project10

---
+++ tenth.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("menu-link").on("click",function(){
+        $(".menu-link").removeClass("active");
+        $(this).addClass("active");
+
+    });
+
+    $(".menu-item").on("mouseover",function(){
+        $(this).addClass("active").siblings().removeClass("active")
+    })
+});
